66d0dbea811e9ee35cd4bb5c60ff50dd30ae427c
davitostes <davisiqueira591@gmail.com>
2024-07-19 18:08:19 -0300

    fix: invalid container name

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 sudo docker rm -f motobot
 sudo docker rmi -f motobot:latest
 sudo docker build -t motobot:latest .
-sudo docker run --name motobot:latest -d -p 8080:8080 motobot
+sudo docker run --name motobot -d -p 8080:8080 motobot
 echo done!
