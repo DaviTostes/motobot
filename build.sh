c3cc696c6497ea61ef7b79b5070bf19d102dbef5
davitostes <davisiqueira591@gmail.com>
2024-07-22 10:10:34 -0300

    fix: update dockerfile

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 git pull
 sudo docker rm -f motobot
-sudo docker rmi -f motobot:latest
-sudo docker build -t motobot:latest .
-sudo docker run --name motobot -d -p 8080:8080 motobot:latest
+sudo docker rmi -f motobot
+sudo docker build -t motobot .
+sudo docker run --name motobot -d  motobot
 sudo docker ps
 echo done!
